76abc9faf93e620dc0b5d50ec1f4928db4dcd540
MasahiroKiji <mkiji@coe.edu>
2020-11-17 09:40:36 -0600

    finish hw10

--- hw10.cu
+++ hw10.cu
@@ -12,9 +12,9 @@ __global__ void initArray( int *A) {
   A[tid] = tid;
 }
 
-__global__ void swapArray( int *A, int size) {
-
-  for(int i=0; i<size/2; i++){
+__global__ void swapArray( int *A, int size, int num_t) {
+   int tid = blockIdx.x * blockDim.x + threadIdx.x;
+  for(int i=size/2/num_t*tid; i<size/2/num_t*(tid+1); i++){
     int temp = A[i];
     A[i] = A[size-1-i];
     A[size-1-i] = temp;
@@ -26,19 +26,21 @@ int main(){
 
   srand(time(0));
   int size=16*1024*1024;
-  // int size=16;
+  //int size=16;
   int* ary = new int[size];
   int* ans = new int[size];
   int num;
   for(int i=0; i<size; i++){
-    num = rand()%100;
+    num = rand();
     ary[i] = num;
     ans[size-i-1] = num;
   }
 
-  int *h_a, *d_a;
+  int *d_a;
   // define thread hierarchy
-  int num_blocks = 8; int num_th_per_blk = 8;
+  int num_blocks = 8; int num_th_per_blk = 16;
+  int num_t = num_blocks*num_th_per_blk;
+
   // allocate host and device memory
   size_t memSize;
   // memSize = num_blocks * num_th_per_blk * sizeof(int);
@@ -50,18 +52,19 @@ int main(){
   // launch kernel
   dim3 dimGrid(num_blocks);
   dim3 dimBlock(num_th_per_blk);
-  // initArray<<< dimGrid, dimBlock >>>(d_a);
-  swapArray<<< dimGrid, dimBlock >>>(d_a, size);
+ 
+  swapArray<<< dimGrid, dimBlock >>>(d_a, size, num_t);
   // retrieve results
   cudaMemcpy( ary, d_a, memSize, cudaMemcpyDeviceToHost);
 
   for(int i=0; i<size; i++){
     if(ary[i]!=ans[i]){
-      cout << ans[i] << " "<< ary[i] << endl;
+      cout << i << " "<< ans[i] << " "<< ary[i] << endl;
       cout << "not match" << endl;
       break;
   }
-  
+//	cout << ans[i] << " "<<ary[i] <<endl;
+
 }
 
   return 0;
